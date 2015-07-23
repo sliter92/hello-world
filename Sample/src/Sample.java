83e6ab1ffacb4e25f8aa11ebbd32d4b3deac74b9
sliter92 <stevenguayaquil@gmail.com>
2015-07-23 17:39:59 -0400

    Testing commit branch #1

---
+++ Sample/src/Sample.java
@@ -0,0 +1,21 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(countHi("codecozecote"));
+	}
+	
+	public static int countHi(String str){
+		char[] c = str.toCharArray();
+		int l = c.length;
+		int answer = 0;
+		
+		for(int i=0;i<l-3;i++){
+			if(c[i] == 'c' && c[i+1]=='o' && c[i+3]=='e'){
+				answer++;
+			}
+		}
+		return answer;
+		}
+}
